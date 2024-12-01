f2a25b3fbf8dfb020755bc925d52fe3d536683de
Cameron Barnes <cameron_barnes@outlook.com>
2024-11-30 21:25:37 -0800

    completed day 1, part 2

--- day-1/src/part2.rs
+++ day-1/src/part2.rs
@@ -1,7 +1,34 @@
+use ahash::{HashMap, HashMapExt};
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> String {
-    todo!("day-1 - part 2")
+pub fn process(input: &str) -> String {
+    let (left, right): (Vec<usize>, Vec<usize>) = input
+        .lines()
+        .map(|line| {
+            let mut iter = line.split_whitespace();
+            (
+                iter.next().unwrap().parse::<usize>().unwrap(),
+                iter.next().unwrap().parse::<usize>().unwrap(),
+            )
+        })
+        .unzip();
+    let mut map: HashMap<usize, usize> = HashMap::new();
+    let mut freq: HashMap<usize, usize> = HashMap::new();
+    left.iter().for_each(|num| {
+        map.insert(*num, 0);
+        freq.entry(*num).and_modify(|freq| *freq += 1).or_insert(1);
+    });
+    right.iter().for_each(|num| {
+        map.entry(*num).and_modify(|val| *val += 1);
+    });
+
+    map.iter()
+        .map(|(key, value)| {
+            let freq = freq.get(key).unwrap_or(&0);
+            *key * *value * freq
+        })
+        .sum::<usize>()
+        .to_string()
 }
 
 #[cfg(test)]
@@ -10,8 +37,12 @@ mod tests {
 
     #[test]
     fn test_process() {
-        todo!("Havent built test yet");
-        let input = "";
-        assert_eq!("", process(input));
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!("31", process(input));
     }
 }

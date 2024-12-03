5707e5b7562940621a881e9f2a36591b373668f7
Cameron Barnes <cameron_barnes@outlook.com>
2024-12-03 01:22:40 -0800

    improved testing for day 3 and marginally optimized part 2

--- day-3/src/part1.rs
+++ day-3/src/part1.rs
@@ -14,4 +14,10 @@ mod tests {
         let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
         assert_eq!("161", process(input));
     }
+
+    #[test]
+    fn test_input() {
+        let input = include_str!("../input.txt");
+        assert_eq!("189600467", process(input));
+    }
 }
--- day-3/src/part2.rs
+++ day-3/src/part2.rs
@@ -5,9 +5,9 @@ pub fn process(input: &str) -> String {
     let mut first = input.split("don't()");
     let mut sum = parse_str(first.next().unwrap());
     first.for_each(|dont| {
-        dont.split("do()").skip(1).for_each(|allowed| {
-            sum += parse_str(allowed);
-        })
+        if let Some((_, valid)) = dont.split_once("do()") {
+            sum += parse_str(valid);
+        }
     });
     sum.to_string()
 }
@@ -20,5 +20,11 @@ mod tests {
     fn test_process() {
         let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
         assert_eq!("48", process(input));
+    }
+
+    #[test]
+    fn test_input() {
+        let input = include_str!("../input.txt");
+        assert_eq!("107069718", process(input));
     }
 }

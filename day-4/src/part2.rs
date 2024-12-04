137aa8f0e43c11164766926c1a1c29b0ed38c402
Cameron Barnes <cameron_barnes@outlook.com>
2024-12-03 21:47:29 -0800

    completed day 4, part 2

--- day-4/src/part2.rs
+++ day-4/src/part2.rs
@@ -1,17 +1,61 @@
+use itertools::Itertools;
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> String {
-    todo!("day-4 - part 2")
+pub fn process(input: &str) -> String {
+    let grid = input.lines().map(|line| line.as_bytes()).collect_vec();
+    let width = grid[0].len();
+    grid.windows(3)
+        .map(|window| {
+            (0..width)
+                .filter(|col_index| {
+                    let first = window[0].iter().skip(*col_index).take(3).collect_vec();
+                    let second = window[1].iter().skip(*col_index).take(3).collect_vec();
+                    let third = window[2].iter().skip(*col_index).take(3).collect_vec();
+                    if first.len() == 3 && second.len() == 3 && third.len() == 3 {
+                        matches!(
+                            (first[0], second[1], third[2]),
+                            (b'M', b'A', b'S') | (b'S', b'A', b'M')
+                        ) && matches!(
+                            (first[2], second[1], third[0]),
+                            (b'M', b'A', b'S') | (b'S', b'A', b'M')
+                        )
+                    } else {
+                        false
+                    }
+                })
+                .count()
+        })
+        .sum::<usize>()
+        .to_string()
 }
 
 #[cfg(test)]
 mod tests {
+    use rstest::rstest;
+
     use super::*;
 
-    #[test]
-    fn test_process() {
-        todo!("Havent built test yet");
-        let input = "";
-        assert_eq!("", process(input));
+    #[rstest]
+    #[case(
+        "M.S
+.A.
+M.S",
+        "1"
+    )]
+    #[case(
+        ".M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........",
+        "9"
+    )]
+    fn test_process(#[case] input: &str, #[case] expected: &str) {
+        assert_eq!(expected, process(input));
     }
 }

190c397bfc9e6111d8af3890117220785c0bc5f4
Cameron Barnes <cameron_barnes@outlook.com>
2025-01-13 16:31:18 -0800

    completed day 7, part 2

--- day-7/src/part2.rs
+++ day-7/src/part2.rs
@@ -1,7 +1,64 @@
+use itertools::Itertools;
+
+fn solve(result: usize, mut arguments: Vec<usize>) -> bool {
+    if arguments.len() == 1 {
+        result == *arguments.first().unwrap()
+    } else {
+        let first = arguments.pop().unwrap();
+        let second = arguments.pop().unwrap();
+        if first > result || second > result {
+            return false;
+        }
+        let mut product = arguments.clone();
+        product.push(first * second);
+        if !solve(result, product.clone()) {
+            product.pop();
+            product.push(first + second);
+            if !solve(result, product.clone()) {
+                product.pop();
+                product.push(
+                    (first.to_string() + &second.to_string())
+                        .parse::<usize>()
+                        .unwrap(),
+                );
+                solve(result, product)
+            } else {
+                true
+            }
+        } else {
+            true
+        }
+    }
+}
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> String {
-    todo!("day-7 - part 2")
+pub fn process(input: &str) -> String {
+    let equations: Vec<(usize, Vec<usize>)> = input
+        .lines()
+        .map(|line| {
+            let (result, arguments) = line.split_once(": ").unwrap();
+            let arguments = arguments
+                .split_ascii_whitespace()
+                .map(|val| val.parse::<usize>().unwrap())
+                .rev()
+                .collect_vec();
+            (result.parse().unwrap(), arguments)
+        })
+        .collect();
+
+    equations
+        .into_iter()
+        .map(
+            |(result, arguments)| {
+                if solve(result, arguments) {
+                    result
+                } else {
+                    0
+                }
+            },
+        )
+        .sum::<usize>()
+        .to_string()
 }
 
 #[cfg(test)]
@@ -10,8 +67,15 @@ mod tests {
 
     #[test]
     fn test_process() {
-        todo!("Havent built test yet");
-        let input = "";
-        assert_eq!("", process(input));
+        let input = "190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20";
+        assert_eq!("11387", process(input));
     }
 }
